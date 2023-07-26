db220468d7b4a7d4f6497f2be8abd5caa414f3f1
KieraConway <66643785+KieraConway@users.noreply.github.com>
2023-07-26 13:46:27 -0700

    Dynamic Complete, Updated Greedy

--- kp_dynamic.py
+++ kp_dynamic.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 """""""""""""""""""""""""""""""""""""""""""""
-    Code 1 - Greedy Algorithm
+    Code 2 - Dynamic Algorithm
 
     Assignment  5 -  Algorithm Report
     Due         July 26, 2023
@@ -23,7 +23,8 @@ from prettytable import PrettyTable     # display data in table format
 n = -1                  # number of items
 max_capacity = -1       # knapsack capacity
 knapsack = []           # knapsack contents
-
+knap_weight = 0         # knapsack weight after added items
+knap_value = 0          # knapsack value after added items
 
 """ ===== Defining Script Classes ===== """
 
@@ -32,72 +33,77 @@ knapsack = []           # knapsack contents
 # Class:        Item
 #
 # Purpose:      __init__ -  initialize Item object parameters
-#               update_knap_status - updates if item is in knapsack
 #
 class Item:
 
-    def __init__(self, number, weight, value, in_knapsack=False):
+    def __init__(self, number, weight, value):
         """ Initialize Item object """
         self.number = number
         self.weight = weight
         self.value = value
-        self.efficiency = value / weight if weight != 0 else -1
-        self.in_knapsack = in_knapsack
-
-    def update_knap_status(self, status):
-        """ Update Item Knapsack Status """
-        self.in_knapsack = status
 
 
 """ ===== Defining Script Functions ===== """
-#
-# Function:     ext_greedy_knapsack()
-#
-# Purpose:      Solves the knapsack problem using the Extended Greedy Algorithm
-#
-# Parameters:   item_list - A list of Items available for the knapsack
-#
-# Return:       current_weight - Total weight of the items in knapsack
-#               current_value  - Total value of the items in knapsack
-#
-# Side Effect:  global variable 'knapsack' is updated to store items
-#
-def ext_greedy_knapsack(item_list):
+
+
+def dynamic_knapsack(item_list):
+    """ Function Initialization """
     # Initialize Variables
+    DP = []                 # DP optimal solution table
     current_weight = 0
     current_value = 0
+    global knapsack, knap_weight, knap_value     # declare modified globals
 
-    # Iterate List of Items
-    for each in item_list:
+    # Initialize DP Table
+    for i in range(n + 1):
+        # Create a capacity row and init to 0
+        DP.append([0 for each in range(max_capacity + 1)])
 
-        # If Adding Current Item Does Not Exceed Capacity
-        if each.weight + current_weight <= max_capacity:
+    """ Bellman Recursion: Fill in DP table """
+    for each_item in range(1, n+1):
+        for each_capacity in range(max_capacity+1):
+            current_weight = item_list[each_item - 1].weight
+            current_value = item_list[each_item - 1].value
 
-            # Add Item to Knapsack
-            knapsack.append(each)
-            each.update_knap_status(True)
+            # If Adding Current Item Exceeds Capacity
+            if each_capacity < current_weight:
+                # Set cell to the previous best solution
+                DP[each_item][each_capacity] = DP[each_item - 1][each_capacity]     # current cell = above cell
 
-            # Update Knapsack Values
-            current_weight += each.weight
-            current_value += each.value
+            else:
+                # Set cell to max(cell above current cell, best value for the remaining capacity)
+                DP[each_item][each_capacity] = max(DP[each_item - 1][each_capacity],
+                                                   DP[each_item - 1][each_capacity - current_weight] + current_value)
 
-    # Return Total Knapsack Values
-    return current_weight, current_value
+    """ Reconstruction: Determine items in the knapsack """
+    current_item = n                    # set current item to last cell
+    current_capacity = max_capacity     # set current capacity to last cell
 
+    # Iterate backwards through table
+    while current_item > 0 and current_capacity > 0:
+
+        # If current cell is different from cell above
+        if DP[current_item][current_capacity] != DP[current_item-1][current_capacity]:
+
+            # included current_item in knapsack
+            knapsack.append(item_list[current_item - 1])            # append item to knapsack list
+            knap_weight += item_list[current_item - 1].weight    # update weight
+            knap_value += item_list[current_item - 1].value      # update value
+            current_capacity -= item_list[current_item - 1].weight  # update capacity
+
+        current_item -= 1   # decrement cell
+
+    """ Return optimal solution value """
+    return DP[n][max_capacity]
 
 """ ===== Main Script Starts Here ===== """
-
-#
-# Function:     main()
-#
-# Purpose:      The driver function for the extended greedy algorithm.
-#                   - initializes the list of items
-#                   - determines the knapsack capacity,
-#                   - sorts list in descending order by efficiency
-#                   - call extended greedy algorithm to solve kp
-#                   - formats and prints result table
-#
 if __name__ == '__main__':
+
+    """
+    #
+    # Program Initialization
+    #
+    """
 
     ''' Initial Variables '''
     # Create Table Objects and Define Headings
@@ -120,26 +126,27 @@ if __name__ == '__main__':
         items_list.append(item)
 
     ''' Set Capacity '''
-    # Caclulate Total and Max Weights
+    # Calculate Total and Max Weights
     total_weight = sum(item.weight for item in items_list)
     max_weight = max(item.weight for item in items_list)
 
     # Set Capacity to Greater Value
     max_capacity = max(int(total_weight * 0.6), max_weight)
 
-    ''' Sort List in Descending Order '''
-    # Sort Items by Efficiency in Descending Order
-    items_list = sorted(items_list, key=attrgetter('efficiency'), reverse=True)
+    """
+    #
+    # Dynamic Programming Algorithm
+    #
+    """
+    ''' Call DP Function '''
+    optimal_value = dynamic_knapsack(items_list)
 
-    ''' Call Extended Greedy Algorithm to Solve KP '''
-    knap_weight, knap_value = ext_greedy_knapsack(items_list)
 
     """
     #
     # Program Termination
     #
     """
-
     ''' Add Item Values to Tables '''
     # Table for All Items
     for each in items_list:
@@ -169,7 +176,6 @@ if __name__ == '__main__':
           f"{'item' if len(knapsack) == 1 else 'items'} "
           f"added to the Knapsack: ")
 
-    knap_tbl.sortby = "Item"
     knap_tbl.vrules = 0
     knap_result = knap_tbl.get_string()
     print(knap_result)
